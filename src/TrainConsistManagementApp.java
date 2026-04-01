2a3521201de85dece751ccb3517abd02cdeabe20
Nisheed Kumar Rout <nisheedkumarrout@gmail.com>
2026-04-01 14:05:42 +0530

    UC1: Display Consist Summary

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,3 +1,11 @@
-void main() {
-    
+public class TrainConsistManagementApp {
+    public static void main(String[] args) {
+
+        String trainName = "Chennai Express";
+        int totalBogies = 0;
+
+        System.out.println("Train Name: " + trainName);
+        System.out.println("Total Bogies: " + totalBogies);
+        System.out.println("Status: Train initialized successfully");
+    }
 }
