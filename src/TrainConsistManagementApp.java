b192f5f308ba5bf7890f4ba70c4f1dc3b92774b6
Nisheed Kumar Rout <nisheedkumarrout@gmail.com>
2026-04-01 14:18:56 +0530

    UC4: Maintain Ordered Bogie IDs using LinkedList

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.LinkedList;
 
 public class TrainConsistManagementApp {
     public static void main(String[] args) {
@@ -30,13 +31,9 @@ public class TrainConsistManagementApp {
         System.out.println(passengerBogies);
 
 
-        // =========================
         // UC3: Unique Bogie IDs (HashSet)
-        // =========================
-
         HashSet<String> bogieIds = new HashSet<>();
 
-        // Adding bogie IDs (with duplicates)
         bogieIds.add("BG101");
         bogieIds.add("BG102");
         bogieIds.add("BG103");
@@ -45,5 +42,35 @@ public class TrainConsistManagementApp {
 
         System.out.println("\nUnique Bogie IDs:");
         System.out.println(bogieIds);
+
+
+        // =========================
+        // UC4: Ordered Train Consist (LinkedList)
+        // =========================
+
+        LinkedList<String> trainConsist = new LinkedList<>();
+
+        // Add bogies
+        trainConsist.add("Engine");
+        trainConsist.add("Sleeper");
+        trainConsist.add("AC");
+        trainConsist.add("Cargo");
+        trainConsist.add("Guard");
+
+        System.out.println("\nInitial Train Consist:");
+        System.out.println(trainConsist);
+
+        // Insert Pantry Car at position 2
+        trainConsist.add(2, "Pantry");
+
+        System.out.println("\nAfter Adding Pantry at position 2:");
+        System.out.println(trainConsist);
+
+        // Remove first and last
+        trainConsist.removeFirst();
+        trainConsist.removeLast();
+
+        System.out.println("\nFinal Train Consist:");
+        System.out.println(trainConsist);
     }
 }
